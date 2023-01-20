173427faafe423e7dc2eaceef878a1cf28bc620b
Camille Aubert <aubertcam@gmail.com>
2023-01-20 11:17:53 +0100

    newroute2

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/newroute', function () {
+Route::get('/newroute2', function () {
     return view('welcome');
 });
