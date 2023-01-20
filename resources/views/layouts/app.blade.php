9af2715c4038ac000c62c06020491d274e46e206
CamiJi <aubertcam@gmail.com>
2023-01-20 16:29:24 +0100

    Compiler les assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel Formation</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
+    <h1 class="text-amber-500 font-bold underline">
+        Hello friends!
+      </h1>
 @include('partials.navbar') 
-@yield('content')
+
+<div class="container mx-auto">
+    @yield('content')
+</div>
 
 
 </body>
